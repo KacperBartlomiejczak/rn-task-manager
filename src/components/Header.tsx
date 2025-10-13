cedc8ebccf2ec9042e258bd71e6ef2c1c6a03b96
KacperBartlomiejczak <kacperbartlomiejczak@proton.me>
2025-10-13 08:46:10 +0200

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="text-left">
+      <h1 className="font-bold text-lg md:text-2xl lg:text-4xl mb-2">
+        Start Your Day & Be Productive👌
+      </h1>
+      <p className="text-gray-500 font-light md:text-lg">
+        Track your progress and build consistent habits
+      </p>
+    </div>
+  );
+}
